f124da443a0c9a445fd40da83b0736d3150ab0c1
Brunozotareli <zotarelibruno@gmail.com>
2025-08-28 21:37:30 -0300

    Model criada

---
+++ BZF/src/main/java/com/Bruno/BZF/BzfApplication.java
@@ -0,0 +1,13 @@
+package com.Bruno.BZF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BzfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BzfApplication.class, args);
+	}
+
+}
---
+++ BZF/src/test/java/com/Bruno/BZF/BzfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bruno.BZF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BzfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
