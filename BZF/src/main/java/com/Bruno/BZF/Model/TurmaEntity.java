dc7941e91e1333c0d235c3a950ed8446df818010
Brunozotareli <zotarelibruno@gmail.com>
2025-08-28 22:16:19 -0300

    atributos entity criados com constructor getters e setters

--- BZF/src/main/java/com/Bruno/BZF/Model/TurmaEntity.java
+++ BZF/src/main/java/com/Bruno/BZF/Model/TurmaEntity.java
@@ -11,4 +11,46 @@ public class TurmaEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    private String curso;
+    private char turma;
+    private int semestre;
+
+    public TurmaEntity(long id, String curso, char turma, int semestre) {
+        this.id = id;
+        this.curso = curso;
+        this.turma = turma;
+        this.semestre = semestre;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public char getTurma() {
+        return turma;
+    }
+
+    public void setTurma(char turma) {
+        this.turma = turma;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
 }
