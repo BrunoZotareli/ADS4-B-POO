8b5e27927930fc39502e468021218292035aaf3e
Brunozotareli <zotarelibruno@gmail.com>
2025-08-28 22:22:18 -0300

    turma repository criada

---
+++ BZF/src/main/java/com/Bruno/BZF/Repository/TurmaRepository.java
@@ -0,0 +1,7 @@
+package com.Bruno.BZF.Repository;
+
+import com.Bruno.BZF.Model.TurmaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TurmaRepository extends JpaRepository <TurmaEntity, Long>{
+}
