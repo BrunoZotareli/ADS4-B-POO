0a71f2a716690daa8de660a2de73f90523c54b5b
Brunozotareli <zotarelibruno@gmail.com>
2025-08-28 21:47:07 -0300

    aluno entity criada

--- BZF/src/main/java/com/Bruno/BZF/Model/AlunoEntity.java
+++ BZF/src/main/java/com/Bruno/BZF/Model/AlunoEntity.java
@@ -2,12 +2,45 @@ package com.Bruno.BZF.Model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class AlunoEntity {
-@Id
-    @GeneratedValue(strategy = )
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    private String nome;
+    private int idade;
+
+    public AlunoEntity(long id, String nome, int idade) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
