abd45ae720951ae3f799fd3623937865e970d2fd
Brunozotareli <zotarelibruno@gmail.com>
2025-08-28 22:20:59 -0300

    aluno repository criada

---
+++ BZF/src/main/java/com/Bruno/BZF/Repository/AlunoRepository.java
@@ -0,0 +1,8 @@
+package com.Bruno.BZF.Repository;
+
+import com.Bruno.BZF.Model.AlunoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository extends JpaRepository<AlunoEntity, Long> {
+
+}
